15a71b5dc8991bbc29432a7e547074216a47cc71
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-09 11:49:10 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/edsonalexandre/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.edsonalexandre.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edsonalexandre.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+
+}
