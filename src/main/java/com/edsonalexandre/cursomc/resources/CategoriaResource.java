7973b2ee210e2ac6fb4cd08096ed1d57e1d22653
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-14 20:51:01 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/edsonalexandre/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edsonalexandre/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,11 @@
 package com.edsonalexandre.cursomc.resources;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,8 +38,23 @@ public class CategoriaResource {
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll(){
 		List<Categoria> list = service.findAll();
-		List<CategoriaDTO> listDTO = list.stream().map((Categoria categoria)-> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		//List<CategoriaDTO> listDTO = list.stream().map((Categoria categoria)-> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		List<CategoriaDTO> listDTO= new ArrayList<>();
+		for(Categoria categoria : list) {			
+			listDTO.add(new CategoriaDTO(categoria));
+		}
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@GetMapping (value="/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+		@RequestParam (value="page", defaultValue = "0") Integer page, 
+		@RequestParam (value="linesPerPage", defaultValue = "24") Integer linesPerPage, 
+		@RequestParam (value="direction", defaultValue = "ASC") String direction, 
+		@RequestParam (value="orderBy", defaultValue = "nome") String orderBy){
+		Page<Categoria> pageable = service.findPage(page, linesPerPage, direction, orderBy);
+		Page<CategoriaDTO> pageableDTO = pageable.map(cat -> new CategoriaDTO(cat));
+		return ResponseEntity.ok().body(pageableDTO);
 	}
 	
 	@PostMapping
