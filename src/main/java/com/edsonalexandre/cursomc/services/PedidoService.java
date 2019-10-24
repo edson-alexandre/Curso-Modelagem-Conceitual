8033e56829ddb4de6baa883d6ba386aeef1c80ff
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-24 14:49:43 -0300

    Implementando toString de pedido

--- src/main/java/com/edsonalexandre/cursomc/services/PedidoService.java
+++ src/main/java/com/edsonalexandre/cursomc/services/PedidoService.java
@@ -28,6 +28,8 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id);
@@ -40,8 +42,8 @@ public class PedidoService {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
-		obj.getPagamento().setPedido(obj);
-		
+		obj.getPagamento().setPedido(obj);		
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamento(pagto, obj.getInstante());			
@@ -51,10 +53,12 @@ public class PedidoService {
 		
 		for(ItemPedido item : obj.getItens()) {
 			item.setDesconto(0.0);
-			item.setPreco(produtoService.find(item.getProduto().getId()).getPreco());
+			item.setProduto(produtoService.find(item.getProduto().getId()));
+			item.setPreco(item.getProduto().getPreco());			
 			item.setPedido(obj);
 		}
-		itemPedidoRepository.saveAll(obj.getItens());		
+		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 	
