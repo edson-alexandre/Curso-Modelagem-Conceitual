7bf5edee821a522573a2ff98d1f3d8f46c683bec
Edson Alexandre <edson.alexandre@outlook.com>
2019-10-28 15:51:35 -0300

    Implementando autenticação e geração do token JWT

---
+++ src/main/java/com/edsonalexandre/cursomc/security/UserSS.java
@@ -0,0 +1,73 @@
+package com.edsonalexandre.cursomc.security;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.edsonalexandre.cursomc.domain.enuns.Perfil;
+
+public class UserSS implements UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String email;
+	private String senha;
+	public Collection<? extends GrantedAuthority> grantedAuthority; 
+	
+	public Integer getId() {
+		return this.id;
+	}
+	
+	public UserSS() {		
+	}
+	
+	
+	
+	public UserSS(Integer id, String email, String senha, Set<Perfil> perfis) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.grantedAuthority = perfis.stream().map(x -> new SimpleGrantedAuthority(x.getDescricao())).collect(Collectors.toList());
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return grantedAuthority;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
